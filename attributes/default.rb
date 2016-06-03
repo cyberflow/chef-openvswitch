956e193b828127b9be6085199797e14f58a9d64f
Vil Surkin <vills@servers.com>
2016-06-03 16:39:51 +0300

    removed recipe 'default'; bump version to 1.0.0 support ubuntu 16.04

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
-default['openvswitch']['packages'] = %w(openvswitch-datapath-dkms openvswitch-common openvswitch-switch)
+default['openvswitch']['packages'] = if node['platform_version'].to_f >= 16.04
+  ['openvswitch-common', 'openvswitch-switch']
+else
+  ['openvswitch-datapath-dkms', 'openvswitch-common', 'openvswitch-switch']
+end
--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'openvswitch'
-maintainer       'WEBZILLA'
-maintainer_email 'dr@webzilla.com'
+maintainer       'Servers.com Cloud Team'
+maintainer_email 'cloud@servers.com'
 license          'Apache 2.0'
 description      'Installs/Configures openvswitch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
-supports 'ubuntu'
+supports 'ubuntu', '>= 14.04'
